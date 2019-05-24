1d4b3bb36a7c173a453c8005a105f047b5d13fa3
KaiserPhemi <oluwafemi.akinwa@andela.com>
2019-05-24 20:51:30 +0100

    ch(add-routes)
    - add default routes
    - create folder structure

---
+++ src/api/v1/index.js
@@ -0,0 +1,10 @@
+// third-party libraries
+const express = require("express");
+
+// main app router
+const appRouter = express.Router();
+
+// routes
+
+// mount routes
+// appRouter.use("/users", usersRoutes);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 // third-party libraries
 const express = require("express");
 
-// instantiate app
+// app
 const app = express();
+const appRouter = require("./api/v1/index");
 
 // middle-wares
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.disable("x-powered-by");
 
 // routes
+app.use("/api/v1", appRouter);
 app.get("/", (req, res) => {
   res.status(200).send({ message: "Welcome to the park-well API" });
 });
