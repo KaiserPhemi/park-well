9d3370613a8c41edcbc678aef5b0b0affdccc096
KaiserPhemi <oluwafemi.akinwa@andela.com>
2019-05-30 01:57:40 +0100

    ch(parking-space-route-test)
    - add more test suites
    
    [Finishes #166290344]

--- src/fixtures/testFixtures.js
+++ src/fixtures/testFixtures.js
@@ -7,6 +7,14 @@ const testFixtures = {
     phoneNumber: "09033445241",
     password: "oluwafemi"
   },
+  secondUser: {
+    firstName: "Victor",
+    lastName: "Phemi",
+    email: "phemi@gmail.com",
+    department: "Engineering",
+    phoneNumber: "09033445241",
+    password: "oluwafemi"
+  },
   newCar: {
     regNo: "MAM-KI9",
     brand: "BMW",
